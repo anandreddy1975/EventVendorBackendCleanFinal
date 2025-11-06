baada6f7b1e3088f17f9e21ef6c452caa22eb711
Anandeeshwara Reddy Ashannagari <anandreddy1975@gmail.com>
2025-11-06 17:33:57 +0530

    Render test version without Gemini API

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return jsonify({"message": "✅ Flask backend running successfully on Render!"})
+    return jsonify({
+        "message": "✅ Flask backend running successfully on Render!"
+    })
 
 if __name__ == '__main__':
-    import os
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host='0.0.0.0', port=5000)
